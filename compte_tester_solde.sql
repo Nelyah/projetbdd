b63e5456b89110a712cd77ffbae02d67eb515942
Jean Desravines <jean.desravines@gmail.com>
2013-12-26 21:35:10 +0100

    Create compte_tester_solde.sql
    Trigger pour le solde du compte

---
+++ compte_tester_solde.sql
@@ -0,0 +1,18 @@
+--
+-- TRIGGER test solde
+--
+
+CREATE OR REPLACE FUNCTION compte_tester_solde() 
+RETURNS trigger AS $$
+BEGIN
+    IF NEW.solde < NEW.decouvert_auto_banque THEN
+        RAISE EXCEPTION 'solde < decouvert autorisé par la banque';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER compte_tester_solde 
+BEFORE INSERT OR UPDATE ON comptes
+FOR EACH ROW EXECUTE PROCEDURE compte_tester_solde();
