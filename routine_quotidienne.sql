f80c4674febdb5cdeeba1e70b7c43bbaad961ba8
Jean Desravines <jean.desravines@gmail.com>
2014-01-04 23:25:49 +0100

    Update routine_quotidienne.sql

--- routine_quotidienne.sql
+++ routine_quotidienne.sql
@@ -6,6 +6,7 @@ CREATE OR REPLACE FUNCTION routine_quotidienne()
 RETURNS VOID AS $$
 DECLARE
 
+	-- Recuperation de tous les virements periodique
 	c_virements CURSOR FOR
 		SELECT id, source_id, destination_id, montant, jour, periode
 		FROM virements_periodique
@@ -31,44 +32,54 @@ BEGIN
 		FETCH c_virements INTO r_virement;
 		EXIT WHEN NOT FOUND;
 
+		-- Recuperation du montant forfaitaire d'execution du virement
 		SELECT forfait_virement_periodique
 		INTO v_forfait
 		FROM types_compte, comptes
 		WHERE types_compte.id = comptes.type_compte_id
 		AND comptes.id = r_virement.source_id;
 		
+		-- Recuperation du solde apres l'execution du virement et applciation des frais
 		SELECT solde - (v_forfait + r_virement.montant)
 		INTO v_solde
 		FROM comptes
 		WHERE comptes.id = r_virement.source_id;
 
+		-- Recuperation du decouvert autorisé par la banque  pour tester la 
+		-- possibilité d'effectuer le virement
 		SELECT decouvert_auto_banque
 		INTO v_decouvert_auto_banque
 		FROM comptes
 		WHERE id = r_virement.source_id;
 
+		-- Recuperation de l'id correspondant à une operation de type 'forfait virement'
 		SELECT id
 		INTO v_type_operation_forfait
 		FROM types_operation
 		WHERE type LIKE 'forfait virement';
 
+		-- Recuperation de l'id correspondant à une operation de type 'virement'
 		SELECT id
 		INTO v_type_operation_virement
 		FROM types_operation
 		WHERE type LIKE 'virement';
 
+		-- Si on peux faire le virement
 		IF (v_solde >= (-v_decouvert_auto_banque)) THEN
 
 			RAISE NOTICE 'viremenet de % vers % pour %e', r_virement.source_id, r_virement.destination_id, r_virement.montant;
 
+			-- Mise a jour du solde source
 			UPDATE comptes SET
 				solde = v_solde
 			WHERE id = r_virement.source_id;
 
+			-- mise a joru du solde destination
 			UPDATE comptes SET
 				solde = solde + r_virement.montant
 			WHERE id = r_virement.destination_id;
 
+			-- enregistrement des l'operation
 			INSERT INTO operations (type_operation_id, source_id, destination_id, montant)
 			VALUES (v_type_operation_virement, r_virement.source_id, r_virement.destination_id, r_virement.montant);
 
